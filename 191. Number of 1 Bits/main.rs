77cad863bca2ea323a27c6b2695cf7f4c744a522
Sagar <chiliverysagar@gmail.com>
2024-09-16 05:16:13 +0530

    Rust -> Optimized solution 191. Number of 1 Bits

--- 191. Number of 1 Bits/main.rs
+++ 191. Number of 1 Bits/main.rs
@@ -1,14 +1,21 @@
-// Approach 1 -> Brute force
+// // Approach 1 -> Brute force
+// fn main() {
+//     let mut num: u8 = 2;
+//     let mut count: u8 = 0;
+
+//     while num > 0 {
+//         if num % 2 == 1 {
+//             count = count + 1;
+//         }
+//         num >>= 1;
+//     }
+
+//     println!("Count -> {count:?}");
+// }
+
+// Approach 2 -> Efficient way
 fn main() {
-    let mut num: u8 = 2;
-    let mut count: u8 = 0;
-
-    while num > 0 {
-        if num % 2 == 1 {
-            count = count + 1;
-        }
-        num >>= 1;
-    }
-
+    let num: i32 = 236;
+    let count = (0..32).fold(0, |acc, exp| acc + ((num & (1 << exp)) >> exp)) as i32;
     println!("Count -> {count:?}");
 }
