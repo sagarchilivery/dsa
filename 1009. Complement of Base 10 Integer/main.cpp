f3456c16eff68570195acf86fd44b38f863e7b03
Sagar <chiliverysagar@gmail.com>
2024-09-16 18:37:06 +0530

    C++ ->1009. Complement of Base 10 Integer || Optimised way

--- 1009. Complement of Base 10 Integer/main.cpp
+++ 1009. Complement of Base 10 Integer/main.cpp
@@ -18,24 +18,53 @@
 
 using namespace std;
 
+// int main()
+// {
+//     int num;
+//     cout << "Enter a number: ";
+//     cin >> num;
+//     vector<int> arr;
+//     int decimal = 0;
+
+//     while (num != 0)
+//     {
+//         arr.push_back(num % 2 == 1 ? 0 : 1);
+//         num >>= 1;
+//     }
+
+//     for (int i = 0; i < arr.size(); i++)
+//     {
+//         decimal = decimal + (arr[i] << i);
+//     }
+//     cout << endl;
+//     cout << "Decimal -> " << decimal;
+// }
+
+/*
+1. Create binary
+1. Compliment of binary
+2. create a mask
+3. AND of compliment & mask
+4. convert it to decimal
+
+
+*/
+
 int main()
 {
-    int num;
-    cout << "Enter a number: ";
-    cin >> num;
-    vector<int> arr;
-    int decimal = 0;
+    int n;
+    cout << "Enter the number -> ";
+    cin >> n;
+    int complement = ~n;
+    int mask = 0;
 
-    while (num != 0)
+    while (n != 0)
     {
-        arr.push_back(num % 2 == 1 ? 0 : 1);
-        num >>= 1;
+        mask = (mask << 1) | 1;
+        n >>= 1;
     }
 
-    for (int i = 0; i < arr.size(); i++)
-    {
-        decimal = decimal + (arr[i] << i);
-    }
-    cout << endl;
-    cout << "Decimal -> " << decimal;
+    int ans = complement & mask;
+
+    cout << ans;
 }
