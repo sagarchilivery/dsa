7109f4703e9bbfbd91d96a80e3900e219653a64d
Sagar <chiliverysagar@gmail.com>
2024-09-19 17:27:38 +0530

    Rust -> 13. Roman to integer || using hashtable

--- 13. Roman to Integer/main.rs
+++ 13. Roman to Integer/main.rs
@@ -1,6 +1,36 @@
+use std::collections::HashMap;
+
 fn main() {
     // let input: &str = "MCDLXXVI";
     // let input = ("MCDLXXVI").to_string();
-    // let input = String::from("MCDLXXVI");
-    // println!("{:?}", input.chars());
+    // let input: String = String::from("MCDLXXVI");
+    let input: String = String::from("MCMXCIV");
+    let mut total: i32 = 0;
+    let mut prev: i32 = 0;
+
+    let roman_map: HashMap<char, i32> = vec![
+        ('I', 1),
+        ('V', 5),
+        ('X', 10),
+        ('L', 50),
+        ('C', 100),
+        ('D', 500),
+        ('M', 1000),
+    ]
+    .into_iter()
+    .collect();
+
+    for i in input.chars() {
+        let value: i32 = *roman_map.get(&i).unwrap();
+
+        if value > prev {
+            total += value - 2 * prev;
+        } else {
+            total += value;
+        }
+
+        prev = value;
+    }
+
+    println!("{:?}", total);
 }
