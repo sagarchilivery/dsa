9d6d31ea9e4b989f64be87b74ed8133985007d17
Sagar <chiliverysagar@gmail.com>
2024-09-29 13:42:06 +0530

    C++ -> 1346. Check if N & its Double Exists || added examples

--- 1346. Check If N and Its Double Exist/main.cpp
+++ 1346. Check If N and Its Double Exist/main.cpp
@@ -1,3 +1,15 @@
+// Example 1:
+
+// Input: arr = [10,2,5,3]
+// Output: true
+// Explanation: For i = 0 and j = 2, arr[i] == 10 == 2 * 5 == 2 * arr[j]
+// Example 2:
+
+// Input: arr = [3,1,7,11]
+// Output: false
+// Explanation: There is no i and j that satisfy the conditions.
+ 
+
 #include <iostream>
 #include <vector>
 #include <unordered_set>
