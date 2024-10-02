2a7a2812f32c6eecf88105b00d969bd49a918c59
Sagar <chiliverysagar@gmail.com>
2024-10-02 19:22:02 +0530

    C++ 283. Move Zeroes || 2nd approach

--- 283. Move Zeroes/main.cpp
+++ 283. Move Zeroes/main.cpp
@@ -6,27 +6,41 @@ using namespace std;
 int main()
 {
     vector<int> nums = {0, 1, 0, 3, 12};
-    int i = 0;
-    int j = 1;
 
-    while (j < nums.size())
+    // Approach 2
+    int i = 0;
+
+    for (int j = 0; j < nums.size(); j++)
     {
-        if (nums[i] != 0)
+        if (nums[j] != 0)
         {
+            swap(nums[j], nums[i]);
             i++;
-            j++;
-        }
-        else if (nums[j] == 0)
-        {
-            j++;
-        }
-        else if (nums[i] == 0 && nums[j] != 0)
-        {
-            int temp = nums[i];
-            nums[i] = nums[j];
-            nums[j] = temp;
         }
     }
+
+    // Arrpoach 1 -> Two pointer approach
+    // int i = 0;
+    // int j = 1;
+
+    // while (j < nums.size())
+    // {
+    //     if (nums[i] != 0)
+    //     {
+    //         i++;
+    //         j++;
+    //     }
+    //     else if (nums[j] == 0)
+    //     {
+    //         j++;
+    //     }
+    //     else if (nums[i] == 0 && nums[j] != 0)
+    //     {
+    //         int temp = nums[i];
+    //         nums[i] = nums[j];
+    //         nums[j] = temp;
+    //     }
+    // }
 
     for (int i : nums)
     {
