9938a232dc845bbaea82fa9887da3218e745abfb
Sagar <chiliverysagar@gmail.com>
2024-10-03 23:15:04 +0530

    C++ 1752. Check if Array is sorted and rotated

---
+++ 1752. Check if Array Is Sorted and Rotated/main.cpp
@@ -0,0 +1,51 @@
+// Example 1:
+
+// Input: nums = [3,4,5,1,2]
+// Output: true
+// Explanation: [1,2,3,4,5] is the original sorted array.
+// You can rotate the array by x = 3 positions to begin on the the element of value 3: [3,4,5,1,2].
+// Example 2:
+
+// Input: nums = [2,1,3,4]
+// Output: false
+// Explanation: There is no sorted array once rotated that can make nums.
+// Example 3:
+
+// Input: nums = [1,2,3]
+// Output: true
+// Explanation: [1,2,3] is the original sorted array.
+// You can rotate the array by x = 0 positions (i.e. no rotation) to make nums.
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+    vector<int> nums = {3, 4, 5, 1, 2};
+    int count = 0;
+    int n = nums.size();
+
+    for (int i = 1; i < nums.size(); i++)
+    {
+        if (nums[i - 1] > nums[i])
+        {
+            count++;
+        }
+    }
+
+    if (nums[n - 1] > nums[0])
+    {
+        count++;
+    }
+
+    if (count <= 1)
+    {
+        cout << "True";
+    }
+    else
+    {
+        cout << "False";
+    }
+}
