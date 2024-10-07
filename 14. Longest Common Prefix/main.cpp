1374578aab56fb59e90666b39f4c32445cac9e44
Sagar <chiliverysagar@gmail.com>
2024-10-07 18:40:21 +0530

    C++ -> 14. Longest Common Prefix

--- 14. Longest Common Prefix/main.cpp
+++ 14. Longest Common Prefix/main.cpp
@@ -18,23 +18,48 @@ int main()
 {
     vector<string> strs = {"flower", "flow", "flight"};
 
-    sort(strs.begin(), strs.end());
+    // Approach 1
+    // sort(strs.begin(), strs.end());
 
-    string first = strs[0];
-    string last = strs[strs.size() - 1];
-    string result = "";
+    // string first = strs[0];
+    // string last = strs[strs.size() - 1];
+    // string result = "";
 
-    for (int i = 0; i < min(first.length(), last.length()); i++)
+    // for (int i = 0; i < min(first.length(), last.length()); i++)
+    // {
+    //     if (first[i] != last[i])
+    //     {
+    //         break;
+    //     }
+    //     else
+    //     {
+    //         result += first[i];
+    //     }
+    // }
+
+    // cout << "Result -> " << result;
+
+    // Approach 2
+    string sample = strs[0];
+    int sample_length = sample.length();
+
+    for (int i = 0; i < strs.size(); i++)
     {
-        if (first[i] != last[i])
+        string s = strs[i];
+        while (sample_length > s.length() || sample != s.substr(0, sample_length))
         {
-            break;
-        }
-        else
-        {
-            result += first[i];
+            sample_length--;
+            if (sample_length == 0)
+            {
+                cout << "Longest Common prefix -> \"\"";
+                // break;
+                return 0;
+            }
+
+            sample = sample.substr(0, sample_length);
         }
     }
 
-    cout << "Result -> " << result;
+    cout << "Longest Common prefix ->" << sample;
+    return 0;
 }
