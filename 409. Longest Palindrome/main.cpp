9aa320cb2b9fa6f049c5d2ff8b7534188f43a7c7
sagar <chiliverysagar@gmail.com>
2024-10-21 04:08:35 +0530

    C++ --> 409. Longest Palindrome || Hashmap Solution (Not optimal)

---
+++ 409. Longest Palindrome/main.cpp
@@ -0,0 +1,53 @@
+// Example 1:
+
+// Input: s = "abccccdd"
+// Output: 7
+// Explanation: One longest palindrome that can be built is "dccaccd", whose length is 7.
+// Example 2:
+
+// Input: s = "a"
+// Output: 1
+// Explanation: The longest palindrome that can be built is "a", whose length is 1.
+
+// Constraints:
+
+// 1 <= s.length <= 2000
+// s consists of lowercase and/or uppercase English letters only.
+
+#include <iostream>
+#include <unordered_map>
+
+using namespace std;
+
+int main()
+{
+    string s = "civilwartestingwhetherthatnaptionoranynartionsoconceivedandsodedicatedcanlongendureWeareqmetonagreatbattlefiemldoftzhatwarWehavecometodedicpateaportionofthatfieldasafinalrestingplaceforthosewhoheregavetheirlivesthatthatnationmightliveItisaltogetherfangandproperthatweshoulddothisButinalargersensewecannotdedicatewecannotconsecratewecannothallowthisgroundThebravelmenlivinganddeadwhostruggledherehaveconsecrateditfaraboveourpoorponwertoaddordetractTgheworldadswfilllittlenotlenorlongrememberwhatwesayherebutitcanneverforgetwhattheydidhereItisforusthelivingrathertobededicatedheretotheulnfinishedworkwhichtheywhofoughtherehavethusfarsonoblyadvancedItisratherforustobeherededicatedtothegreattdafskremainingbeforeusthatfromthesehonoreddeadwetakeincreaseddevotiontothatcauseforwhichtheygavethelastpfullmeasureofdevotionthatweherehighlyresolvethatthesedeadshallnothavediedinvainthatthisnationunsderGodshallhaveanewbirthoffreedomandthatgovernmentofthepeoplebythepeopleforthepeopleshallnotperishfromtheearth";
+
+    unordered_map<char, int> hashmap;
+
+    for (char c : s)
+    {
+        hashmap[c]++;
+    }
+
+    int count = 0;
+
+    for (auto [key, value] : hashmap)
+    {
+        if (value % 2 == 0)
+        {
+            count += value;
+        }
+        if ((value - 1) % 2 == 0)
+        {
+            count += value - 1;
+        }
+    }
+
+    if (count < s.length())
+    {
+        count += 1;
+    }
+
+    cout << "Longest palindrome is --> " << count << endl;
+}
